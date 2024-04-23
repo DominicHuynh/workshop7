f9fabbdc16897d9231eb813e28d182c121075fa5
DominicHuynh <phucvbin2005@gmail.com>
2024-04-23 20:33:08 +0930

    just a small fix

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -7,8 +7,8 @@
 int main() {
     // Create a House object
     House house1(2);
-    Fridge* fridge1 = new Fridge(100, 300);
-    TV* tv1 = new TV(100, 50);
+    Appliance* fridge1 = new Fridge(100, 300);
+    Appliance* tv1 = new TV(100, 50);
     house1.addAppliance(fridge1);
     house1.addAppliance(tv1);
     std::cout<<"Total power consumption: "<<house1.getTotalPowerConsumption()<<endl;
